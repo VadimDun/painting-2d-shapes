fa377c77e03ddaefc525977e2219a95521f8342e
VadimDun <V_ad_im3704@mail.ru>
2025-11-22 21:56:11 +0300

    task1

--- painting-2d-shapes/main.cpp
+++ painting-2d-shapes/main.cpp
@@ -65,24 +65,68 @@ void InitShader() {
         std::cout << "Could not bind attrib " << attr_name << std::endl;
         return;
     }
+}
 
+// создает вершины правильного пятиугольника
+std::vector<Vertex> createPentagon(float centerX, float centerY, float radius) {
+    std::vector<Vertex> pentagon;
+    for (int i = 0; i < 5; ++i) {
+        float angle = 2.0f * 3.14159f * i / 5.0f;
+       
+        Vertex v;
+        v.x = centerX + radius * cos(angle);
+        v.y = centerY + radius * sin(angle);
+        pentagon.push_back(v);
+    }
+    return pentagon;
 }
 
 void InitVBO() {
-    Vertex triangle[6] = {
-        { -1.0f, -0.5f },
-        {  0.5f, -0.5f },
-        {  0.0f,  0.5f },
-        {  1.0f,  0.5f },
-        {  2.0f,  -0.5f },
-        {  -1.0f,  -1.0f }
+    std::vector<Vertex> quad = {
+        { -0.8f, -0.8f },  // LD
+        { -0.3f, -0.8f },  // RD
+        { -0.3f, -0.3f },  // RU
+
+        { -0.8f, -0.8f },  // LD
+        { -0.3f, -0.3f },  // RU
+        { -0.8f, -0.3f }   // LU
     };
+
+    std::vector<Vertex> fan = {
+        { 0.0f, 0.0f },    // center
+
+        { 0.47f, 0.17f },
+        { 0.41f, 0.32f },
+        { 0.32f, 0.44f },
+        { 0.21f, 0.53f },
+        { 0.07f, 0.57f },
+        { -0.07f, 0.57f },
+        { -0.21f, 0.53f },
+        { -0.32f, 0.44f },
+        { -0.41f, 0.32f },
+        { -0.47f, 0.17f },
+    };
+
+    std::vector<Vertex> pentagonVertices;
+    Vertex center = { 0.5f, -0.5f };
+    std::vector<Vertex> pentagon = createPentagon(center.x, center.y, 0.3f);
+
+    // 5 fans from center
+    for (int i = 0; i < 5; ++i) {
+        pentagonVertices.push_back(center);
+        pentagonVertices.push_back(pentagon[i]);
+        pentagonVertices.push_back(pentagon[(i + 1) % 5]);
+    }
+
+    std::vector<Vertex> allVertices;
+    allVertices.insert(allVertices.end(), quad.begin(), quad.end());
+    allVertices.insert(allVertices.end(), fan.begin(), fan.end());
+    allVertices.insert(allVertices.end(), pentagonVertices.begin(), pentagonVertices.end());
 
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, allVertices.size() * sizeof(Vertex), allVertices.data(), GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, NULL);
-
 }
 
 void Init() {
@@ -97,23 +141,31 @@ void Draw() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(Attrib_vertex, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
+    // quad
     glDrawArrays(GL_TRIANGLES, 0, 6);
-    //glDrawArrays(GL_TRIANGLES, 1, 3);
 
+    // fan
+    glDrawArrays(GL_TRIANGLE_FAN, 6, 11);
+
+    // pentagon
+    glDrawArrays(GL_TRIANGLES, 17, 15);
+
+    glBindBuffer(GL_ARRAY_BUFFER, NULL);
+    glDisableVertexAttribArray(Attrib_vertex);
+    glUseProgram(0);
 }
 
 void Release() {
-    glBindBuffer(GL_ARRAY_BUFFER, NULL);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     glDeleteBuffers(1, &VBO);
-
     glDeleteProgram(Program);
 }
 
 int main() {
     setlocale(LC_ALL, "ru");
 
-    sf::Window window(sf::VideoMode(800, 600), "Green Triangle",
-        sf::Style::Default, sf::ContextSettings(32));
+    // чтобы фигуры не расстягивались, нужно квадратное окно
+    sf::Window window(sf::VideoMode(900, 900), "Shapes", sf::Style::Default, sf::ContextSettings(32));
     window.setVerticalSyncEnabled(true);
     window.setActive(true);
 
